29da90ad2e6a66efc90643c328705f4161274e0d
Posledov Pavel <pavelposledov89@gmail.com>
2023-07-10 20:45:40 +0600

    переделал создание и редактирование задач с классом TodoUpdateView(FormView) и TodoCreateView(FormView)

--- Todoproject/webapp/views.py
+++ Todoproject/webapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render,redirect,get_object_or_404
+from django.urls import reverse_lazy
+
 from .models import TodoList
 from .forms import TodoForm
 from django.http import HttpResponseRedirect,Http404
-from django.views.generic import TemplateView,View
+from django.views.generic import TemplateView, View, FormView
+
 
 # Create your views here.
 class TodoListWiew(TemplateView):
@@ -12,49 +15,63 @@ class TodoListWiew(TemplateView):
         context = {"todos": todos}
         return render(request, 'todo_list.html', context)
 
-class TodoCreateView(View):
-    def get(self, request, *args, **kwargs):
-        form = TodoForm()
-        return render(request, "add_todolist.html", {'form': form})
-    def post(self,request, *args, **kwargs):
-            form = TodoForm(data=request.POST)
-            if form.is_valid():
-                type_todo = form.cleaned_data.pop("type_todo")
-                todo = TodoList.objects.create(
-                    title=form.cleaned_data.get('title'),
-                    status=form.cleaned_data.get('status'),
-                    short_description=form.cleaned_data.get('short_description'),
-                    description=form.cleaned_data.get('description'),)
-                todo.type_todo.set(type_todo)
-                return redirect('home')
-            else:
-                return redirect(request, "add_todolist.html", {'form':form})
-class TodoUpdateView(View):
-    def get(self,request, *args,**kwargs):
-        todo = get_object_or_404(TodoList, id=kwargs['pk'])
-        form = TodoForm(initial={
-            "title": todo.title,
-            "status": todo.status,
-            "type_todo": todo.type_todo.all(),
-            "short_description": todo.short_description,
-            "description": todo.description
-        })
-        return render(request, "update_todo.html", {'form': form})
+class TodoCreateView(FormView):
+    success_url = reverse_lazy("home")
+    form_class = TodoForm
+    template_name = "add_todolist.html"
 
-    def post(self,request,*args,**kwargs):
-            todo = get_object_or_404(TodoList, id=kwargs['pk'])
-            form = TodoForm(data=request.POST)
-            if form.is_valid():
-                type_todo = form.cleaned_data.pop("type_todo")
-                todo.title=form.cleaned_data.get("title")
-                todo.status=form.cleaned_data.get("status")
-                todo.short_description=form.cleaned_data.get("short_description")
-                todo.description=form.cleaned_data.get("description")
-                todo.save()
-                todo.type_todo.set(type_todo)
-                return redirect('home')
-            else:
-                return render(request, "update_todo.html", {'form': form})
+    def form_valid(self, form):
+        type_todo = form.cleaned_data.pop("type_todo")
+        todo = TodoList.objects.create(
+            title=form.cleaned_data.get('title'),
+            status=form.cleaned_data.get('status'),
+            short_description=form.cleaned_data.get('short_description'),
+            description=form.cleaned_data.get('description'), )
+        todo.type_todo.set(type_todo)
+        return super().form_valid(form)
+
+
+class TodoUpdateView(FormView):
+    form_class = TodoForm
+    template_name = "update_todo.html"
+    def dispatch(self, request, *args, **kwargs):
+        self.todo = self.get_object(kwargs.get("pk"))
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_object(self, pk):
+        return get_object_or_404(TodoList, id=pk)
+
+
+    def get_initial(self):
+        initial = {}
+        for key in 'title', 'status', 'type_todo', 'short_description', 'description':
+            initial[key] = getattr(self.todo, key)
+        initial['type_todo'] = self.todo.type_todo.all()
+        return initial
+
+
+    def form_valid(self, form):
+        type_todo = form.cleaned_data.pop("type_todo")
+        for key, value in form.cleaned_data.items():
+            if value is not None:
+                setattr(self.todo, key, value)
+        self.todo.save()
+        self.todo.type_todo.set(type_todo)
+        return redirect("home")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 class TodoDeleteView(TemplateView):
     def get(self, request, *args, **kwargs):
