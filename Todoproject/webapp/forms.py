3e6baf4e35e85c47a55dde84d5fb64e858710fac
Posledov Pavel <pavelposledov89@gmail.com>
2023-07-10 21:59:32 +0600

    Написал и задействовал два любых кастомных валидатора на любые поля

--- Todoproject/webapp/forms.py
+++ Todoproject/webapp/forms.py
@@ -1,6 +1,15 @@
 from django import forms
+from django.core.exceptions import ValidationError
+
 from .models import TodoList,TypeTodo,StatusTodo
 
+def at_least_10(value):
+    if len(value)<10:
+        raise ValidationError('Это значение слишком короткое')
+
+def len_50(value):
+    if len(value)>50:
+        raise ValidationError('Это значение слишком длинное')
 
 class StatusTodoForm(forms.Form):
     title = forms.CharField(max_length=50, required=True, label="Название")
@@ -15,6 +24,8 @@ class TypeTodoForm(forms.Form):
 
 
 class TodoForm(forms.ModelForm):
+    title = forms.CharField(max_length=50, required=True, label='Название', validators=[at_least_10])
+    short_description = forms.CharField(max_length=50, required=True, label='Заголовок', validators=[len_50])
 
     class Meta:
         model = TodoList
