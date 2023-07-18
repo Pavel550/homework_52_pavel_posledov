7ea61ff061ba59ad47dcf536e217b41b942e31a9
Posledov Pavel <pavelposledov89@gmail.com>
2023-07-18 18:49:46 +0600

    изменил в задачах создание редактиирование удаление без подтверждения и детальный просмотр с помощью CreateView DetailView UpdateView DeleteView

--- Todoproject/webapp/views/todo_views.py
+++ Todoproject/webapp/views/todo_views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 from webapp.models import TodoList, Project
 from webapp.forms import TodoForm
 from django.http import HttpResponseRedirect,Http404
-from django.views.generic import TemplateView, View, FormView, ListView, CreateView
+from django.views.generic import TemplateView, View, FormView, ListView, CreateView, DetailView, DeleteView, UpdateView
 
 
 # Create your views here.
@@ -15,11 +15,6 @@ class TodoListView(ListView):
     ordering = '-created_date'
 
 
-    # def get(self, request, *args, **kwargs):
-    #
-    #     todos = TodoList.objects.order_by("-created_date")
-    #     context = {"todos": todos}
-    #     return render(request, 'todo/todo_list.html', context)
 
 class TodoCreateView(CreateView):
     template_name= 'todo/add_todolist.html'
@@ -32,67 +27,57 @@ class TodoCreateView(CreateView):
 
 
 
-    # def form_valid(self, form):
-    #     type_todo = form.cleaned_data.pop("type_todo")
-    #     todo = TodoList.objects.create(
-    #         title=form.cleaned_data.get('title'),
-    #         status=form.cleaned_data.get('status'),
-    #         short_description=form.cleaned_data.get('short_description'),
-    #         description=form.cleaned_data.get('description'), )
-    #     todo.type_todo.set(type_todo)
-    #     return super().form_valid(form)
 
+class TodoUpdateView(UpdateView):
 
-class TodoUpdateView(FormView):
+    model = TodoList
+
+    template_name ='todo/update_todo.html'
+
     form_class = TodoForm
-    template_name = "todo/update_todo.html"
-    def dispatch(self, request, *args, **kwargs):
-        self.todo = self.get_object(kwargs.get("pk"))
-        return super().dispatch(request, *args, **kwargs)
 
-    def get_object(self, pk):
-        return get_object_or_404(TodoList, id=pk)
+    context_object_name ='todo'
 
 
-    def get_initial(self):
-        initial = {}
-        for key in 'title', 'status', 'type_todo', 'short_description', 'description':
-            initial[key] = getattr(self.todo, key)
-        initial['type_todo'] = self.todo.type_todo.all()
-        return initial
+    def get_success_url(self):
 
-
-    def form_valid(self, form):
-        type_todo = form.cleaned_data.pop("type_todo")
-        for key, value in form.cleaned_data.items():
-            if value is not None:
-                setattr(self.todo, key, value)
-        self.todo.save()
-        self.todo.type_todo.set(type_todo)
-        return redirect("home")
+        return reverse('detail_project',kwargs={'pk':self.object.project.pk})
 
 
 
-class TodoDeleteView(TemplateView):
+
+
+
+
+class TodoDeleteView(DeleteView):
+
+    model = TodoList
+
+
     def get(self, request, *args, **kwargs):
-        todo = get_object_or_404(TodoList, id=kwargs['pk'])
-        return render(request, "todo/delete_todo.html", {"todo": todo})
+
+        return self.delete(request, *args, **kwargs)
 
 
-    def post(self,*args,**kwargs):
-        todo = get_object_or_404(TodoList,id=kwargs['pk'])
-        todo.delete()
-        return redirect('home')
+    def get_success_url(self):
 
-    def get_template_names(self,**kwargs):
-        return "delete_todo.html"
+        return reverse('detail_project',kwargs={'pk':self.object.project.pk})
 
 
 
-class TodoDetailView(TemplateView):
+
+
+class TodoDetailView(DetailView):
+    model = TodoList
+    template_name = "todo/detail_todo.html"
+
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["todo"] = get_object_or_404(TodoList, id=kwargs['pk'])
+        context["tasks"] = self.object
+        print(object)
+        # for i in self.object.tasks.all():
+        #     print(i, i.created_date)
         return context
-    def get_template_names(self):
-        return "detail_todo.html"
+
+
