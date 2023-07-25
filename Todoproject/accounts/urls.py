f0061ea95e7336b4c1057438ace7495dc265b924
Posledov Pavel <pavelposledov89@gmail.com>
2023-07-25 16:43:28 +0600

    создал формы в акаунтах.добавил методы для регистрации юзеров в проекты.так же сделал валидацию на фамилию или имя чтобы какое то поле было заполненное

--- Todoproject/accounts/urls.py
+++ Todoproject/accounts/urls.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from accounts.views import login_view, logout_view
+from accounts.views import login_view, logout_view, register_view
 
 app_name = "accounts"
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name="logout"),
+    path('create/', register_view, name="create"),
 
 
 
--- Todoproject/accounts/views.py
+++ Todoproject/accounts/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 
+from accounts.forms import ProjectUserCreationForm
+
 
 # Create your views here.
 def login_view(request):
@@ -11,6 +13,9 @@ def login_view(request):
         user = authenticate(request, username = username, password = password)
         if user is not None:
             login(request, user)
+            next = request.GET.get("next")
+            if next:
+                return redirect(next)
             return redirect('webapp:home')
         else:
             context['has_error'] = True
@@ -21,3 +26,15 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('webapp:home')
+
+
+def register_view(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = ProjectUserCreationForm(data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('webapp:home')
+    else:
+        form = ProjectUserCreationForm()
+    return render(request, 'user_create.html', context={'form': form})
